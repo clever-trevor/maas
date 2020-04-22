c2fd67d543dac62661088141f711a26a99eb55af
work@clevertrevor.com <work@clevertrevor.com>
2020-04-22 14:58:25 +0100

    STandardised field names and added more clikc-throughs

--- maas/install/install.py
+++ maas/install/install.py
@@ -14,8 +14,8 @@ maas.read('/app/maas/conf/env')
 
 def delete_indices ():
   x = elasticsearch.delete_index(es,"maas_config_fragments")
-  x = elasticsearch.delete_index(es,"maas_config_hosts_publish")
-  x = elasticsearch.delete_index(es,"maas_config_hosts_require")
+  x = elasticsearch.delete_index(es,"maas_config_entity_publish")
+  x = elasticsearch.delete_index(es,"maas_config_entity_require")
   x = elasticsearch.delete_index(es,"maas_config_alert")
   x = elasticsearch.delete_index(es,"maas_alert_log_current")
   x = elasticsearch.delete_index(es,"maas_alert_log_history")
@@ -28,9 +28,9 @@ def create_indices ():
 
   settings = {"settings":{ "number_of_shards" : 1, "number_of_replicas":0 }, "mappings":{ "properties":{"last_updated":{"type":"date","format":"dd/MM/yyyy HH:mm:ss"}}}}
   # Fully populated config per host
-  x = elasticsearch.create_index(es,"maas_config_hosts_publish",settings)
+  x = elasticsearch.create_index(es,"maas_config_entity_publish",settings)
   # Custom monitoring requirements
-  x = elasticsearch.create_index(es,"maas_config_hosts_require",settings)
+  x = elasticsearch.create_index(es,"maas_config_entity_require",settings)
   # Alert settings
   x = elasticsearch.create_index(es,"maas_config_alert",settings)
 
@@ -54,9 +54,7 @@ def create_symlinks():
  
 es = { "url" : maas['elastic']['url'], "user" : maas['elastic']['user'], "pass" : maas['elastic']['pass'] }
 
-delete_indices()
+#delete_indices()
 time.sleep(2)
 create_indices()
 create_symlinks()
-
-
