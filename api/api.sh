897f1088723a8080d79e1623907c797f2cf4d88c
work@clevertrevor.com <work@clevertrevor.com>
2020-04-30 09:55:51 +0100

    Added stop to api/www scripts

--- api/api.sh
+++ api/api.sh
@@ -1,3 +1,4 @@
 cd /app/maas/api
+ps -ef | grep "python3.*api.py" | grep -v grep | awk '{print $2}' | xargs kill
 nohup python3 /app/maas/api/api.py > /dev/null 2>&1 &
 
--- www/www.sh
+++ www/www.sh
@@ -1,3 +1,4 @@
 cd /app/maas/www
+ps -ef | grep "python3.*www.py" | grep -v grep | awk '{print $2}' | xargs kill
 nohup python3 /app/maas/www/www.py > /dev/null 2>&1 &
 
