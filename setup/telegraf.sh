fe7f81bccf35b6a2df38a71194cb75b3551e35a4
work@clevertrevor.com <work@clevertrevor.com>
2020-04-30 10:15:55 +0100

    Added a getting started guide

--- setup/telegraf.sh
+++ setup/telegraf.sh
@@ -2,4 +2,4 @@ ps -ef | grep -v grep | grep telegraf | awk '{print $2}' | xargs kill >/dev/null
 
 host=`hostname | sed -e 's/\..*//g'`
 cd /app/telegraf
-nohup /app/telegraf/telegraf/usr/bin/telegraf --config "http://192.168.1.113/cgi-bin/telegraf-configure?host=$host&os=linux&reset=true" >/dev/null 2>&1 &
+nohup /app/telegraf/telegraf/usr/bin/telegraf --config "http://192.168.1.113:9001/telegraf-configure?host=$host&os=linux&reset=true" >/dev/null 2>&1 &
