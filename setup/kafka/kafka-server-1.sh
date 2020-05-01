b594d7d2ec39fa14e68f836c82ba03925c932b7b
trev <work@clevertrevor.com>
2020-05-01 15:07:35 +0100

    Fix kafka startup script

--- setup/kafka/kafka-server-1.sh
+++ setup/kafka/kafka-server-1.sh
@@ -2,4 +2,4 @@ cd /app/kafka/kafka
 
 ps -ef | grep "kafka-server-1.sh" | grep -v grep | awk '{print $2}' | xargs kill >/dev/null 2>&1
 
-nohup bin/kafka-server-start.sh /app/kafka/conf/server-1.properties &
+nohup bin/kafka-server-start.sh /app/kafka/conf/kafka-server-1.properties &
