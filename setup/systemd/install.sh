0ec25628afdd6c610aa74f45814ff5a9d9ff048e
trev <work@clevertrevor.com>
2020-05-06 08:10:42 +0100

    User substitution for systemd files

--- setup/systemd/install.sh
+++ setup/systemd/install.sh
@@ -1,4 +1,23 @@
-cp * /etc/systemd/system
+echo "Enter username for the system daemons"
+read USER
+
+if [ -z "$USER" ]
+then 
+  echo "You need supply a username"
+  exit
+fi
+
+SYSTEMD=/etc/systemd/system
+sed "s/%USER%/$USER/g" chronograf.service > $SYSTEMD/chronograf.service
+sed "s/%USER%/$USER/g" elasticsearch.service > $SYSTEMD/elasticsearch.service
+sed "s/%USER%/$USER/g" grafana.service > $SYSTEMD/grafana.service
+sed "s/%USER%/$USER/g" influxdb.service > $SYSTEMD/influxdb.service
+sed "s/%USER%/$USER/g" kafka.service > $SYSTEMD/kafka.service
+sed "s/%USER%/$USER/g" maas-api.service > $SYSTEMD/maas-api.service
+sed "s/%USER%/$USER/g" maas-www.service > $SYSTEMD/maas-www.service
+sed "s/%USER%/$USER/g" telegraf.service > $SYSTEMD/telegraf.service
+sed "s/%USER%/$USER/g" zookeeper.service > $SYSTEMD/zookeeper.service
+exit
 systemctl daemon-reload
 systemctl enable chronograf 
 systemctl enable elasticsearch
